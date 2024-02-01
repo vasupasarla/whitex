2b6996a81a29da403c854e3749daebc77a30e6aa
vasupasarla <v.pasarla@zensar.com>
2024-02-01 17:51:17 +0530

    first x

---
+++ build/app/generated/source/buildConfig/debug/com/example/white_x/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.white_x;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.white_x";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
